7f3337392ba11970a9d6e240376e37f2f3bd125a
Kamil Tekliński <kteklinski@ntrc.eu>
2017-04-24 18:58:56 +0200

    add svg field onInit with event capturing click coords

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import * as d3 from "d3";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,26 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
   styleUrls: ['./map.css']
 })
 export class MapComponent implements OnInit {
+
+  private w = window.innerWidth;
+  private h = window.innerHeight;
+  private holder = d3.select("body")
+    .append("svg")
+    .attr("class", "floor-map")
+    .attr("width", this.w)
+    .attr("height", this.h);
+
+  ngOnInit(): void {
+    this.initMap();
+    this.translate.setDefaultLang('en');
+  }
+
   constructor(private router: Router,
               private route: ActivatedRoute,
               public translate: TranslateService) {
   }
 
-  ngOnInit(): void  {
-    this.newMap();
-    this.translate.setDefaultLang('en');
-  }
-
-  private newMap(): void {
+  private initMap(): void {
+    let floorMap = d3.select("svg.floor-map").on("click", () => { console.log(d3.event)});
   }
 }
