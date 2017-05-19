7dadd183d10a8c37edd0440ef16fe239bcb331c5
pkuzmicki <pkuzmicki@blastlab.co>
2017-05-19 10:28:13 +0200

    WIP: replacing input with text after setting scale

--- src/app/map/toolbar/tools/scale/scale.ts
+++ src/app/map/toolbar/tools/scale/scale.ts
@@ -3,8 +3,8 @@ import {ToolsEnum} from '../tools.enum';
 import * as d3 from 'd3';
 import {Tool} from '../tool';
 import {TranslateService} from '@ngx-translate/core';
-import {Scale} from './scale.type';
-import {Line, Point} from "../../../map.type";
+import {MeasureEnum, Scale} from './scale.type';
+import {Line, Point} from '../../../map.type';
 // import {Scale} from './scale.type';
 
 @Component({
@@ -15,10 +15,12 @@ import {Line, Point} from "../../../map.type";
 export class ScaleComponent implements Tool {
   @Output() clickedTool: EventEmitter<Tool> = new EventEmitter<Tool>();
   public hintMessage: String = 'Click at map to set scale.';
-  private scaleArray: Scale;  // use for scale data
+  private scale: Scale;  // use for scale data
   private pointsArray: Array<Point> = [];
   private linesArray: Array<Line> = [];
   public active: boolean = false;
+  public isScalaDisplayed: boolean = false;
+  public isScalaSet: boolean = false;
   public toolEnum: ToolsEnum = ToolsEnum.SCALE; // used in hint-bar component as a toolName
   private line;
   private start;
@@ -51,14 +53,19 @@ export class ScaleComponent implements Tool {
   }
 
   private hideScale(): void {
+    this.isScalaDisplayed = false;
+
     d3.select('#map').style('cursor', 'default');
-    console.log(d3.select('#pointGroup'));
     d3.select('#pointGroup').remove();
-    console.log(d3.select('#pointGroup'));
+    d3.select('#mapBg').on('click', null);
   }
 
   private startDrawingScale = (): void => {
     const scaleComponent = this;
+    document.getElementById('map-container').addEventListener("scroll", this.onScroll);
+
+    // console.log((<HTMLInputElement>document.getElementById('scaleInput')));
+    d3.select('#scaleInput').attr('top', '0px');
 
     const mapBg = d3.select('#mapBg');
     mapBg.style('cursor', 'crosshair');
@@ -70,16 +77,19 @@ export class ScaleComponent implements Tool {
       .append('g')
       .attr('id', 'pointGroup');
 
-    console.log(d3.select('#pointGroup'));
     this.redrawPoints();
     this.redrawLine();
   };
+
+  private onScroll = (): void => {
+    this.setInputPosition();
+  }
 
   private addPoint = (): void => {
     const p = <Point>{
       x: d3.event.offsetX,
       y: d3.event.offsetY
-    }
+    };
 
     if (this.start == null) {
       this.pointsArray.push(p);
@@ -89,13 +99,11 @@ export class ScaleComponent implements Tool {
       this.stop = this.redrawPoints();
       const l = this.createLine();
       this.linesArray.push(l);
-      this.redrawLine()
-      // this.line = this.drawLine();
-      // this.createScale();
+      this.redrawLine();
 
       this.createInput();
     }
-  }
+  };
 
   private redrawPoints = (): any => {
     const scaleComponent = this;
@@ -124,65 +132,22 @@ export class ScaleComponent implements Tool {
       .on('mouseout', function () {
         d3.select(this).attr('fill-opacity', 1);
       })
+      .on('click', function (d) {
+        console.log(d3.select(this).datum());
+      })
       .call(d3.drag()
         .on('drag', function () {
           scaleComponent.pointDrag(d3.select(this));
         }));
     return newCircle;
-  }
+  };
 
   private createLine = (): Line => {
     return <Line>{
       p1: this.pointsArray[0],
       p2: this.pointsArray[1]
-    }
-  }
-
-
-  private createScale = (): void => {
-    const p1 = <Point>{
-      x: this.start.attr('cx'),
-      y: this.start.attr('cy')
-    }
-    const p2 = <Point>{
-      x: this.stop.attr('cx'),
-      y: this.stop.attr('cy')
-    }
-    const scale = <Scale>{
-      start: p1,
-      stop: p2,
-      scale: 34,
-      measure: 0
-    }
-  }
-
-  private drawLine() {
-    const scaleComponent = this;
-    const group = d3.select('#pointGroup');
-    const l = group.append('svg:line')
-      .datum(function (d) {
-        d = scaleComponent.pointsArray;
-        return d;
-      })
-      .attr('x1', function (d) {
-        return d[0].x;
-      })
-      .attr('y1', function (d) {
-        return d[0].y;
-      })
-      .attr('x2', function (d) {
-        return d[1].x;
-      })
-      .attr('y2', function (d) {
-        return d[1].y;
-      })
-      .attr('stroke-width', 5)
-      .attr('stroke', 'green')
-      .on('click', function (d) {
-        console.log(d);
-      });
-    return l;
-  }
+    };
+  };
 
 
   private redrawLine = (): void => {
@@ -192,12 +157,8 @@ export class ScaleComponent implements Tool {
     const lines = group.selectAll('line');
     const newLine = lines.data(this.linesArray).enter()
       .append('svg:line')
-      /*.datum(function (d) {
-        d.lines = [];
-        return d;
-      })*/
       .attr('x1', function (d) {
-        console.log(d);
+        scaleComponent.isScalaDisplayed = true;
         return d.p1.x;
       })
       .attr('y1', function (d) {
@@ -210,10 +171,7 @@ export class ScaleComponent implements Tool {
         return d.p2.y;
       })
       .attr('stroke-width', 5)
-      .attr('stroke', 'green')
-      .on('click', function (d) {
-        console.log(d);
-      });
+      .attr('stroke', 'green');
 
     lines
       .attr('x1', function (d) {
@@ -227,33 +185,7 @@ export class ScaleComponent implements Tool {
       })
       .attr('y2', function (d) {
         return d.p2.y;
-      })
-  };
-
-  private createInput = (): void => {
-
-    const x = (parseInt(this.pointsArray[0].x.toString(), 10) + parseInt(this.pointsArray[1].x.toString(), 10)) / 2;
-    const y = (parseInt(this.pointsArray[0].y.toString(), 10) + parseInt(this.pointsArray[1].y.toString(), 10)) / 2;
-    const group = d3.select('#pointGroup');
-    group
-      .append("foreignObject")
-      .attr('id', 'scaleInput')
-      .attr('x', x)
-      .attr('y', y)
-      .append("xhtml:body")
-      .attr('xmlns', 'http://www.w3.org/1999/xhtml')
-      .html('<input type="text" id="scaleInput"/>')
-      .on('mouseout', function () {
-        // scaleComponent.setScale(line);
       });
-  }
-
-  private redrawInput = ():void =>{
-    const x = (parseInt(this.pointsArray[0].x.toString(), 10) + parseInt(this.pointsArray[1].x.toString(), 10)) / 2;
-    const y = (parseInt(this.pointsArray[0].y.toString(), 10) + parseInt(this.pointsArray[1].y.toString(), 10)) / 2;
-    d3.select('#scaleInput')
-      .attr('x', x)
-      .attr('y', y);
   };
 
   private pointDrag = (circle): void => {
@@ -267,4 +199,96 @@ export class ScaleComponent implements Tool {
     this.redrawLine();
     this.redrawInput();
   };
+
+  private createInput = (): void => {
+    const scaleComponent = this;
+    const inputElement = (<HTMLInputElement>document.getElementById('scaleInput'));
+    const scaleInput = d3.select(inputElement);
+    scaleInput.on('keypress', function () {
+      if (d3.event.keyCode === 13) {
+        scaleComponent.submitScale();
+      }
+    });
+    this.setInputPosition();
+  };
+
+  private submitScale = (): void => {
+    const unit = (<HTMLInputElement>document.getElementById('unit')).value;
+    let measure;
+    if (unit === 'cm') {
+      measure = MeasureEnum.CENTIMETERS;
+    } else if (unit === 'm') {
+      measure = MeasureEnum.METERS;
+    }
+    console.log(measure);
+    const length: string = (<HTMLInputElement>document.getElementById('scaleInput')).value;
+    const p1 = <Point>{
+      x: this.start.attr('cx'),
+      y: this.start.attr('cy')
+    };
+    const p2 = <Point>{
+      x: this.stop.attr('cx'),
+      y: this.stop.attr('cy')
+    };
+    this.scale = this.createScale(p1, p2, length, measure);
+    console.log(this.scale);
+    this.isScalaSet = true;
+    this.redrawInput();
+    // this.replaceInputWithText();
+  };
+
+  private createScale = (p1: Point, p2: Point, num: string, unit: number): Scale => {
+    return <Scale>{
+      start: p1,
+      stop: p2,
+      scale: parseFloat(num),
+      measure: unit
+    };
+  };
+
+  /*private replaceInputWithText = (): void => {
+   const scaleInputParent = document.getElementById('scaleInput').parentNode;
+   const scaleInput = document.getElementById('scaleInput');
+   // document.get
+   console.log(d3.select(scaleInputParent));
+   let text = document.createTextNode('Siema');
+   console.log(text);
+   console.log(d3.select(text));
+   // console.log(document.getElem('#text'));
+
+   scaleInputParent.replaceChild(text, scaleInput);
+   };*/
+
+  private redrawInput = (): void => {
+    this.setInputPosition();
+  };
+
+  private setInputPosition = (): void => {
+    let scale;
+    if (!this.isScalaSet) {
+      console.log('scala is not set');
+      scale = (<HTMLInputElement>document.getElementById('scaleInput'));
+    } else {
+      console.log('scala is set');
+      scale = document.getElementById('scaleText');
+      d3.select(scale).attr('textContent', 'siema')
+      console.log(d3.select(scale));
+    }
+
+    const unitSelect = (<HTMLInputElement>document.getElementById('unit'));
+    const mapToolbar = document.getElementsByClassName('map-toolbar');
+    const canvas = document.getElementById('map-container');
+
+    const x = (parseInt(this.linesArray[0].p1.x.toString(), 10) + parseInt(this.linesArray[0].p2.x.toString(), 10)) / 2 + 110 - 30 - canvas.scrollLeft;
+    const y = (parseInt(this.linesArray[0].p1.y.toString(), 10) + parseInt(this.linesArray[0].p2.y.toString(), 10)) / 2 - 30 - canvas.scrollTop;
+
+
+    scale.style.top = y.toString(10) + 'px';
+    scale.style.left = x.toString(10) + 'px';
+    scale.style.width = '70px';
+    unitSelect.style.top = y.toString(10) + 'px';
+    unitSelect.style.left = (x + 75).toString(10) + 'px';
+  };
+
+
 }
