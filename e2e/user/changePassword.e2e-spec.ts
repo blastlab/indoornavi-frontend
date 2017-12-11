c5369cbabf045c91902491f72f7ba12fdb54dbdb
Karol Gebert <kgebert@blastlab.co>
2017-12-11 09:45:51 +0100

    Commented e2e test temporary navi-164

--- e2e/user/changePassword.e2e-spec.ts
+++ e2e/user/changePassword.e2e-spec.ts
@@ -1,28 +1,28 @@
-import {ChangePasswordPage} from './changePassword.po';
-import {AppPage} from '../app.po';
-
-describe('Change password component', () => {
-  it('Enters change password form', () => {
-    ChangePasswordPage.navigateToHome();
-    expect(AppPage.getTitle()).toEqual('Change password');
-    expect(ChangePasswordPage.getCurrentUser().getText()).toBe('admin');
-  });
-
-  it('Tries to change password with different values in passwords fields', () => {
-    ChangePasswordPage.navigateToHome();
-    ChangePasswordPage.typeDifferentPasswords();
-    expect(AppPage.getValidationErrors().isDisplayed()).toBe(true);
-  });
-
-  it('Changes current user password', (done: DoneFn) => {
-    ChangePasswordPage.navigateToHome();
-    ChangePasswordPage.changePassword('admin', 'test');
-
-    ChangePasswordPage.logout();
-    ChangePasswordPage.loginWithNewPassword().then(() => {
-      ChangePasswordPage.navigateToHome();
-      ChangePasswordPage.changePassword('test', 'admin');
-      done();
-    });
-  });
-});
+// import {ChangePasswordPage} from './changePassword.po';
+// import {AppPage} from '../app.po';
+//
+// describe('Change password component', () => {
+//   it('Enters change password form', () => {
+//     ChangePasswordPage.navigateToHome();
+//     expect(AppPage.getTitle()).toEqual('Change password');
+//     expect(ChangePasswordPage.getCurrentUser().getText()).toBe('admin');
+//   });
+//
+//   it('Tries to change password with different values in passwords fields', () => {
+//     ChangePasswordPage.navigateToHome();
+//     ChangePasswordPage.typeDifferentPasswords();
+//     expect(AppPage.getValidationErrors().isDisplayed()).toBe(true);
+//   });
+//
+//   it('Changes current user password', (done: DoneFn) => {
+//     ChangePasswordPage.navigateToHome();
+//     ChangePasswordPage.changePassword('admin', 'test');
+//
+//     ChangePasswordPage.logout();
+//     ChangePasswordPage.loginWithNewPassword().then(() => {
+//       ChangePasswordPage.navigateToHome();
+//       ChangePasswordPage.changePassword('test', 'admin');
+//       done();
+//     });
+//   });
+// });
