b05a6c852a412f2340cf54a75d3f294658d97d14
motlowski <motlowski@blastlab.co>
2018-01-26 13:51:33 +0100

    [Update] Add new files v.0.9

---
+++ e2e_selenium/e2e/tests/devices/test_base.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from pages.devices.devices_page import DevicesPage
+
+class TestBase:
+
+    def multi_assertion(self):
+        # create list for all returned True or False
+
+        asserts = []
+        dropdown_present = self.construction_page.is_dropdown_button_present()
+        constr_counter = self.construction_page.check_if_there_is_any_row()
+
+        asserts.append(dropdown_present)
+        asserts.append(constr_counter)
+        # Check if there is any False in array
+        return False if False in asserts else True
