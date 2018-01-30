44990bfb0fbc0e8c54ebc7af82e56905b1dadbe4
Pawel Sternicki <psternicki@blastlab.co>
2018-01-30 15:08:09 +0100

    Changed configuration for Docker

--- e2e_selenium/e2e/config.py
+++ e2e_selenium/e2e/config.py
@@ -1,5 +1,14 @@
 class Config:
+
+  def runningInDocker():
+    with open('/proc/self/cgroup', 'r') as procfile:
+      for line in procfile:
+        fields = line.strip().split('/')
+        if 'docker' in fields:
+          return True
+    return False
+
+
+  DOCKER = runningInDocker()
   front_hostname = 'http://localhost:4200/'
   db_hostname = 'localhost'
-  DOCKER = False
-
--- e2e_selenium/e2e/main.py
+++ e2e_selenium/e2e/main.py
@@ -5,11 +5,11 @@ import os
 import sys
 cwd = os.getcwd()
 sys.path.append(cwd)
-#from tests.test_login_page import TestLoginPage
-#from tests.constructions.test_complexes_page import TestComplexesPage
-#from tests.constructions.test_buildings_page import TestBuildingsPage
-#from tests.constructions.test_floors_page import TestFloorsPage
-from tests.constructions.test_map_page import TestMapPage
+from tests.test_login_page import TestLoginPage
+from tests.constructions.test_complexes_page import TestComplexesPage
+from tests.constructions.test_buildings_page import TestBuildingsPage
+from tests.constructions.test_floors_page import TestFloorsPage
+#from tests.constructions.test_map_page import TestMapPage
 if __name__ == '__main__':
-    unittest.main()
-    # unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'),failfast=False, buffer=False, catchbreak=False)
+    # unittest.main()
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'),failfast=False, buffer=False, catchbreak=False)
