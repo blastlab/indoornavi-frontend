f7737bcb8d6542379eb1d8ff0a1a2412d31c8302
Tekliński <kteklinski@blastlab.co>
2018-04-26 16:54:12 +0200

    add todo for refactor changeColor method of SvgGroupWrapper

--- src/app/shared/utils/drawing/drawing.builder.ts
+++ src/app/shared/utils/drawing/drawing.builder.ts
@@ -199,6 +199,7 @@ export class SvgGroupWrapper {
     this.group.select('rect.group-border-box').remove();
   }
 
+  // TODO: Refactor this method to be more specific - remember about usages
   changeColor(newColor) {
     const parentElement: SVGElement = this.group.node();
     const childrenCount: number = parentElement.childElementCount;
