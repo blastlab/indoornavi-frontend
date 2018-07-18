7707d65fb8f77ee8f126ebefc5938f63815b4fde
bartoshko <blenart@blastlab.co>
2018-07-18 16:38:42 +0200

    device-placer.component drawing sink from configuration

--- src/app/map-editor/tool-bar/tools/devices/device-placer/device-placer.component.ts
+++ src/app/map-editor/tool-bar/tools/devices/device-placer/device-placer.component.ts
@@ -6,11 +6,15 @@ import * as d3 from 'd3';
 import {MapLoaderInformerService} from '../../../../../shared/services/map-loader-informer/map-loader-informer.service';
 import {Scale, ScaleCalculations, ScaleDto} from '../../scale/scale.type';
 import {Geometry} from '../../../../../shared/utils/helper/geometry';
-import {Anchor, Sink} from '../../../../../device/device.type';
 import {Configuration} from '../../../../action-bar/actionbar.type';
-import {Helper} from '../../../../../shared/utils/helper/helper';
 import {ActionBarService} from '../../../../action-bar/actionbar.service';
 import {ScaleService} from '../../../../../shared/services/scale/scale.service';
+import {ZoomService} from '../../../../../shared/services/zoom/zoom.service';
+import {SinkInEditor} from '../../../../../map/models/sink';
+import {AnchorInEditor} from '../../../../../map/models/anchor';
+import {Sink} from '../../../../../device/device.type';
+import {DeviceInEditorConfiguration} from '../../../../../map/models/device';
+import {Point} from '../../../../map.type';
 
 @Component({
   selector: 'app-device-placer',
@@ -30,7 +34,8 @@ export class DevicePlacerComponent implements Tool, OnInit, OnDestroy {
   constructor(
     private mapLoaderInformer: MapLoaderInformerService,
     private configurationService: ActionBarService,
-    private scaleService: ScaleService
+    private scaleService: ScaleService,
+    private zoomService: ZoomService,
   ) { }
 
   ngOnInit() {
@@ -82,63 +87,24 @@ export class DevicePlacerComponent implements Tool, OnInit, OnDestroy {
 
   private fetchConfiguredDevices(): void {
     this.configurationService.configurationLoaded().first().subscribe((configuration: Configuration): void => {
-      const config: Configuration = Helper.deepCopy(configuration);
-      this.floorId = config.floorId;
-      if (!!config.data.sinks) {
-        console.log(config.data.sinks);
-        //   const sinks: Sink[] = [];
-      //   config.data.sinks.forEach((sink: Sink): void => {
-      //     sinks.push(<Sink>this.recalculateDeviceCoordinatesFromCentimetersToPixels(sink))
-      //   });
-      //   this.drawSinksAndConnectedAnchors(sinks);
+      this.floorId = configuration.floorId;
+      if (!!configuration.data.sinks) {
+        configuration.data.sinks.forEach((sink: Sink) => {
+          const onMapCoordinates: Point = Geometry.calculatePointPositionInPixels(
+            this.scaleCalculations.scaleLengthInPixels,
+            this.scaleCalculations.scaleInCentimeters,
+            {x: sink.x, y: sink.y});
+          const drawConfiguration: DeviceInEditorConfiguration = {
+            id: 'aaa',
+            clazz: 'aa',
+            heightInMeters: Geometry.calculateDistanceInPixels(this.scaleCalculations.scaleLengthInPixels, this.scaleCalculations.scaleInCentimeters, sink.z)
+          };
+          const sinkOnMap: SinkInEditor = new SinkInEditor(onMapCoordinates , this.map, drawConfiguration);
+
+        });
       }
-      if (!!config.data.anchors) {
-        console.log(config.data.anchors);
-        // const anchors: Anchor[] = [];
-        // config.data.anchors.forEach((anchor: Anchor): void => {
-        //   anchors.push(this.recalculateDeviceCoordinatesFromCentimetersToPixels(anchor));
-        // });
-        // this.drawAnchorsWithoutConnection(anchors);
-      }
-      // this.configuration = config;
-      // this.fetchDevices();
     });
   }
-  // private fetchDevices(): void {
-  //   this.verifiedDevices = [];
-  //   this.deviceService.setUrl('sinks/');
-  //   this.deviceService.getAll().subscribe((sinks: Sink[]): void => {
-  //     sinks.forEach((sink: Sink): void => {
-  //       if (sink.verified) {
-  //         if (!sink.floorId || sink.floorId === this.configuration.floorId) {
-  //           if (this.getIndexOfSinkInConfiguration(sink) >= 0) {
-  //             sink = this.updateSinkFromConfiguration(sink);
-  //           } else {
-  //             sink = <Sink>DevicesComponent.eraseDevicePublicationData(sink);
-  //             this.devicePlacerController.addToRemainingDevicesList(sink);
-  //           }
-  //         }
-  //         this.verifiedDevices.push(sink);
-  //       }
-  //     });
-  //   });
-  //   this.deviceService.setUrl('anchors/');
-  //   this.deviceService.getAll().subscribe((anchors: Anchor[]): void => {
-  //     anchors.forEach((anchor: Anchor): void => {
-  //       if (anchor.verified) {
-  //         if (!anchor.floorId || anchor.floorId === this.configuration.floorId) {
-  //           if (this.getIndexOfAnchorInConfiguration(anchor) >= 0) {
-  //             anchor = this.updateAnchorFromConfiguration(anchor);
-  //           } else {
-  //             anchor = <Sink>DevicesComponent.eraseDevicePublicationData(anchor);
-  //             this.devicePlacerController.addToRemainingDevicesList(anchor);
-  //           }
-  //         }
-  //         this.verifiedDevices.push(anchor);
-  //       }
-  //     });
-  //   });
-  // }
 
 
 }
