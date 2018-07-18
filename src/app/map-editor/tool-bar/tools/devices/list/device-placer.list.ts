240c9414857f1f927e5177c6346b5895357d5ad6
Karol Gebert <kgebert@blastlab.co>
2018-07-18 15:45:42 +0200

    Mock for device-placer list and minimize tool details window (still work in progress) navi-316

---
+++ src/app/map-editor/tool-bar/tools/devices/list/device-placer.list.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-device-placer-list',
+  templateUrl: './devices-list.html'
+})
+export class DevicePlacerListComponent {
+
+}
