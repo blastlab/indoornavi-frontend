59cb3b0e60594e0ca38d5e44287a7ed521d140bc
Marcin Bogucki <mbogucki@blastlab.co>
2018-08-16 15:45:08 +0200

    adding new propereties for devices

--- src/app/device/device.type.ts
+++ src/app/device/device.type.ts
@@ -11,9 +11,12 @@ export class Device {
   yInPixels?: number;
   floorId?: number;
   firmwareVersion?: string;
+  major?: number;
+  macAddress?: string;
+  powerTransmition?: number;
 
   constructor(shortId: number, longId: number, verified: boolean, id?: number, name?: string, x?: number, y?: number, z?: number,
-              floorId?: number, firmwareVersion?: string, xInPixels?: number, yInPixels?: number) {
+              floorId?: number, firmwareVersion?: string, xInPixels?: number, yInPixels?: number, major?: number, macAddress?: string, powerTransmition?: number) {
     this.id = id;
     this.name = name;
     this.shortId = shortId;
@@ -25,6 +28,9 @@ export class Device {
     this.firmwareVersion = firmwareVersion;
     this.xInPixels = xInPixels;
     this.yInPixels = yInPixels;
+    this.major = major;
+    this.macAddress = macAddress;
+    this.powerTransmition = powerTransmition;
   }
 }
 
