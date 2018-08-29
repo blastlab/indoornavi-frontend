93ec474872a1523d887fa1236a91832d68d6e276
Marcin Bogucki <mbogucki@blastlab.co>
2018-08-29 13:23:04 +0200

    changing directive for shortId and adding translation

--- src/app/shared/directive/short-id.directive.ts
+++ src/app/shared/directive/short-id.directive.ts
@@ -7,24 +7,26 @@ import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angu
 })
 export class ShortIdValidatorDirective implements Validator {
   @Input() appProperShortId: string;
+  @Input() translateKey: string;
+
   private message: ValidationErrors = null;
 
-  static checkIsNumber(value: string): ValidationErrors {
-    return /[0-9]/.test(value) ? null : {properShortId: 'device.shortId.mustBeNumber'}
+  static checkIsNumber(value: string, translateKey: string): ValidationErrors {
+    return /[0-9]/.test(value) ? null : {properShortId: `${translateKey}.mustBeNumber`}
   }
 
-  static checkIsInRange(deviceType: string, value: string): ValidationErrors {
+  static checkIsInRange(deviceType: string, value: string, translateKey: string): ValidationErrors {
     if (deviceType === 'tags') {
-      return parseInt(value, 10) < 32768 ? null : {properShortId: 'validator.input.shortIdOutOfRange'};
+      return parseInt(value, 10) < 32768 ? null : {properShortId: `${translateKey}.shortIdOutOfRange`};
     } else {
-      return (parseInt(value, 10) > 32767 && parseInt(value, 10) < 2147483648) ? null : {properShortId: 'validator.input.shortIdOutOfRange'};
+      return (parseInt(value, 10) > 32767 && parseInt(value, 10) < 2147483648) ? null : {properShortId: `${translateKey}.shortIdOutOfRange`};
     }
   }
 
   validate(control: AbstractControl): ValidationErrors {
-    this.message = ShortIdValidatorDirective.checkIsNumber(control.value);
+    this.message = ShortIdValidatorDirective.checkIsNumber(control.value, this.translateKey);
     if (this.message == null) {
-      this.message = ShortIdValidatorDirective.checkIsInRange(this.appProperShortId, control.value);
+      this.message = ShortIdValidatorDirective.checkIsInRange(this.appProperShortId, control.value, this.translateKey);
     }
     return this.message;
   }
