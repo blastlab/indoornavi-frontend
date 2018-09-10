0da411bf6219a11ddc3880a42b3fc667012f4187
Karol Gebert <kgebert@blastlab.co>
2018-09-10 11:42:16 +0200

    Fixed two bugs described in navi-405

--- src/app/shared/directive/number-in-range.directive.ts
+++ src/app/shared/directive/number-in-range.directive.ts
@@ -19,20 +19,20 @@ export class NumberInRangeValidatorDirective implements Validator {
     const val = parseInt(value, 10);
 
     if (deviceType === 'tags') {
-      if (val < 32678) {
+      if (val > 32767) {
         return {
           properNumberInRange: `${translateKey}.numberInRange`
         };
       }
     } else {
-      if (val > 32767 && val < 2147483648) {
-        return null;
-      } else {
+      if (val < 32768 || val >= 2147483648) {
         return {
           properNumberInRange: `${translateKey}.numberInRange`
         };
       }
     }
+
+    return null;
   }
 
   validate(control: AbstractControl): ValidationErrors {
