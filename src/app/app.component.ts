823d1926bc3d325e796c571cca4bb2e2d6bbbfc7
Marcin Bogucki <mbogucki@blastlab.co>
2018-09-24 15:03:03 +0200

    reseting padding in publication map navi-436

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,28 @@ import {ActivatedRoute, Params} from '@angular/router';
 import {MenuItem} from 'primeng/primeng';
 import {Subscription} from 'rxjs/Subscription';
 import {BreadcrumbService} from './shared/services/breadcrumbs/breadcrumb.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html'
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
   isUserLoggedIn: boolean;
   isDisplayedInIFrame: boolean = false;
   sidebar: boolean = false;
   breadcrumbs: MenuItem[];
+  mapPublication: boolean = false;
   private userLoggedInSubscription: Subscription;
   private breadcrumbIsReadySubscription: Subscription;
 
-  constructor(private authGuard: AuthGuard, private route: ActivatedRoute, private breadcrumbService: BreadcrumbService, private cd: ChangeDetectorRef) {
+  constructor(
+    private authGuard: AuthGuard,
+    private route: ActivatedRoute,
+    private breadcrumbService: BreadcrumbService,
+    private cd: ChangeDetectorRef,
+    private location: Location) {
   }
 
   ngOnInit() {
@@ -38,6 +46,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.cd.detectChanges();
     });
     this.hideSidebar();
+    this.mapPublication = this.location.path().match(/publications/) ? true : false;
   }
 
   ngOnDestroy() {
