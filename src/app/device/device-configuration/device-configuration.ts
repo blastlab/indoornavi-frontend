f21b6a4aa92e5e0c0a62ce569a690b08265a27a6
Marcin Bogucki <mbogucki@blastlab.co>
2018-11-14 09:56:48 +0100

    moving rf config device to separate component navi-509

--- src/app/device/device-configuration/device-configuration.ts
+++ src/app/device/device-configuration/device-configuration.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {SelectItem} from 'primeng/primeng';
 
 @Component({
   selector: 'app-device-configuration',
@@ -10,11 +11,16 @@ export class DeviceConfigurationComponent implements OnInit {
   @Input() deviceType: string;
   @Output() deviceConfigClosed: EventEmitter<boolean> = new EventEmitter<boolean>();
   titleHeader: string;
+  rfsetConfigData;
 
   constructor() {}
 
   ngOnInit() {
     this.titleHeader = `device.details.${this.deviceType}.config`;
+    this.rfsetConfigData = {
+      radioChannels: this.radioChannels,
+      radioBaudRate: this.radioBaudRate
+    };
   }
 
   closeDeviceConfig(): void {
@@ -25,4 +31,22 @@ export class DeviceConfigurationComponent implements OnInit {
     this.closeDeviceConfig();
   }
 
+  private get radioChannels(): SelectItem[] {
+    return [
+      { label: '1', value: 1 },
+      { label: '2', value: 2 },
+      { label: '3', value: 3 },
+      { label: '4', value: 4 },
+      { label: '5', value: 5 },
+      { label: '7', value: 7 }
+    ];
+  }
+
+  private get radioBaudRate(): SelectItem[] {
+    return [
+      { label: '110', value: 110 },
+      { label: '850', value: 850 },
+      { label: '6800', value: 6800 }
+    ];
+  }
 }
---
+++ src/app/device/device-rfset/device-rfset.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {SelectItem} from 'primeng/primeng';
+
+@Component({
+  selector: 'app-device-rfset',
+  templateUrl: './device-rfset.html'
+})
+export class DeviceRfsetComponent implements OnInit {
+
+  @Input() rfsetConfigData;
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log(this.rfsetConfigData);
+  }
+
+}
