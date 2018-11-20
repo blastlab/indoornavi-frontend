9c031bcf353512338ca5f3cbc11d249fb889cbde
Marcin Bogucki <mbogucki@blastlab.co>
2018-11-20 15:36:20 +0100

    adding basic form to imu config devices navi-509

--- src/app/device/device-imu/device-imu.ts
+++ src/app/device/device-imu/device-imu.ts
@@ -1,10 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-device-imu',
   templateUrl: './device-imu.html'
 })
-export class DeviceImuComponent {
+export class DeviceImuComponent implements OnInit {
 
+  imuConfigForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createRfForm();
+  }
+
+  private createRfForm(): void {
+    this.imuConfigForm = this.fb.group({
+      imuData: this.fb.group({
+        toggleImu: false
+      })
+    });
+  }
 }
