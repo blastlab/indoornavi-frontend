6822898856d5e0fd6a5c6108f85ad80e18a7530e
Marcin Bogucki <mbogucki@blastlab.co>
2018-11-30 08:41:05 +0100

    adding form for mac configuration navi-509

--- src/app/device/device-mac/device-mac.ts
+++ src/app/device/device-mac/device-mac.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
@@ -7,6 +7,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class DeviceMacComponent implements OnInit {
 
+  @Input() deviceType: string;
   macConfigForm: FormGroup;
   isButtonSendDisabled = true;
   constructor(private fb: FormBuilder) {}
@@ -17,20 +18,43 @@ export class DeviceMacComponent implements OnInit {
 
   sendToDevice(): void {
     this.setIsButtonDisabled(true);
+    console.log(this.macConfigForm.value);
   }
 
   resetDevice(): void {
     this.setIsButtonDisabled(false);
+    this.macConfigForm.patchValue({
+      macData: {
+        beaconTimerInterval: 4,
+        slotPeriod: (!this.isDeviceTag) ? 7 : null,
+        slotTime: (!this.isDeviceTag) ? 15 : null,
+        guardTime: (!this.isDeviceTag) ? 21 : null,
+        devicePersonalAreaNetwork: 48392,
+        newDeviceAddress: 49283,
+        reportAnchorToAnchorDistances: true
+      }
+    });
   }
 
   private setIsButtonDisabled(isDisabled: boolean): void {
     this.isButtonSendDisabled = isDisabled;
   }
 
+  get isDeviceTag(): boolean {
+    return this.deviceType === 'tags';
+  }
+
   private createMacForm(): void {
+    console.log(this.isDeviceTag);
     this.macConfigForm = this.fb.group({
       macData: this.fb.group({
-
+        beaconTimerInterval: 1,
+        slotPeriod: (!this.isDeviceTag) ? 1 : null,
+        slotTime: (!this.isDeviceTag) ? 2 : null,
+        guardTime: (!this.isDeviceTag) ? 3 : null,
+        devicePersonalAreaNetwork: 46562,
+        newDeviceAddress: 15426,
+        reportAnchorToAnchorDistances: false
       })
     });
   }
