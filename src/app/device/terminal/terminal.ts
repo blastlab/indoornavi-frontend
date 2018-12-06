afe69f445d5e88b52c1a7461985657122328eb13
bartoshko <blenart@blastlab.co>
2018-12-06 10:47:09 +0100

    terminal in new component

---
+++ src/app/device/terminal/terminal.ts
@@ -0,0 +1,23 @@
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  templateUrl: './terminal.html',
+  selector: 'app-terminal-component',
+  encapsulation: ViewEncapsulation.None
+})
+export class TerminalComponent implements OnInit, OnDestroy {
+
+  private
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+}
