50442151511a783ed4a9b8636a1f4a4822fd9e72
Paweł Sternicki <psternicki@blastlab.co>
2018-12-12 13:28:12 +0100

    Fixed websocket url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
+var loc = window.location, ws_addr;
+if (loc.protocol === "https:") {
+  ws_addr = "wss://";
+} else {
+  ws_addr = "ws://";
+}
+
+ws_addr += loc.host;
+
 export const environment = {
   production: true,
   base_url: '',
-  ws_url: 'wss://',
+  ws_url: ws_addr,
   version: require('../../package.json').version
 };
