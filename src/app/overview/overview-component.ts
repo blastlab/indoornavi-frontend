2fd13b06298940166ff347421c4f6c38e3792ac2
bartoshko <blenart@blastlab.co>
2018-12-19 15:59:53 +0100

    minification of image factor and scale influance calculated, navi-534

--- src/app/overview/overview-component.ts
+++ src/app/overview/overview-component.ts
@@ -5,12 +5,13 @@ import {Subject} from 'rxjs/Subject';
 import {FloorService} from '../floor/floor.service';
 import {BreadcrumbService} from '../shared/services/breadcrumbs/breadcrumb.service';
 import {TranslateService} from '@ngx-translate/core';
-import {Scale, ScaleCalculations} from '../map-editor/tool-bar/tools/scale/scale.type';
+import {Scale, ScaleCalculations, ScaleDto} from '../map-editor/tool-bar/tools/scale/scale.type';
 import {Geometry} from '../shared/utils/helper/geometry';
 import {EChartOption} from 'echarts';
 import {MapService} from '../map-editor/uploader/map.uploader.service';
 import {echartHeatmapConfig} from './echart.config';
 import {CoordinatesIncident, CoordinatesRequest, ReportService} from './services/coordinates.service';
+import {Point} from '../map-editor/map.type';
 
 @Component({
   templateUrl: './overview.html'
@@ -26,6 +27,8 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
   private imageUrl: string;
   private imageWidth: number;
   private imageHeight: number;
+  private heatmapImageEdgePoint: Point = {x: 0, y: 0};
+  private scaleMinificationFactor: ScaleDto;
   private scale: Scale;
   private scaleCalculations: ScaleCalculations;
   private subscriptionDestructor: Subject<void> = new Subject<void>();
@@ -63,7 +66,7 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
 
   private calculateEChartOffset(): void {
     this.heatmapOffsetX = this.heatMapWidth * 0.1;
-    this.heatmapOffsetY = this.heatMapHeight * 0.115;
+    this.heatmapOffsetY = this.heatMapHeight * 0.075;
   }
   private setCorrespondingFloorParams(): void {
     this.route.params.takeUntil(this.subscriptionDestructor)
@@ -101,11 +104,13 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
           };
         }
         if (!!floor.imageId) {
+          console.log(floor);
           this.mapService.getImage(floor.imageId).subscribe((blob: Blob) => {
             this.imageUrl = URL.createObjectURL(blob);
             this.loadMapImage().then((imageSize: number[]): void => {
               this.imageHeight = imageSize[0];
               this.imageWidth = imageSize[1];
+              this.calculateDataToHeatmapScaleFactorChange();
               this.loadData();
             });
           });
@@ -114,35 +119,53 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
     });
   }
 
+  private calculateDataToHeatmapScaleFactorChange(): void {
+    const mapSizeInPixels: Point = {
+      x: this.heatmapImageEdgePoint.x - this.heatmapOffsetX,
+      y: this.heatmapImageEdgePoint.y - this.heatmapOffsetY
+    };
+    const sizeMinificationFactor: Point = {
+      x: mapSizeInPixels.x / this.imageWidth,
+      y: mapSizeInPixels.x / this.imageHeight
+    };
+    const stop: Point = {
+      x: Math.abs((this.scale.start.x - this.scale.stop.x) * sizeMinificationFactor.x),
+      y: Math.abs((this.scale.start.y - this.scale.stop.y) * sizeMinificationFactor.y)
+    };
+    this.scaleMinificationFactor = new ScaleDto({x: 0, y: 0}, stop, this.scale.realDistance, this.scale.measure);
+  }
+
   private loadMapImage(): Promise<number[]> {
-    return new Promise((resolve) => {
-      const canvas = document.getElementsByTagName('canvas').item(0);
-      const newCanvas = document.createElement('canvas');
-      newCanvas.setAttribute('id', 'image-heatmap-canvas');
-      canvas.parentElement.appendChild(newCanvas);
-      const ctx = newCanvas.getContext('2d');
-      const background = new Image();
-      background.src = this.imageUrl;
-      const result: number[] = [];
-      let width = 0;
-      let height = 0;
-      const self = this;
-      background.onload = function () {
-        width += background.width;
-        height += background.height;
-        ctx.drawImage(background, self.heatmapOffsetX, self.heatmapOffsetX,
-          self.heatMapWidth - self.heatmapOffsetX * 2, self.heatMapHeight - self.heatmapOffsetY * 2);
-        result.push(width);
-        result.push(height);
-        resolve(result);
-      }.bind(self);
-      newCanvas.setAttribute('width', `${this.heatMapWidth}px`);
-      newCanvas.setAttribute('height', `${this.heatMapHeight}px`);
-    });
+      return new Promise((resolve) => {
+        const canvas = document.getElementsByTagName('canvas').item(0);
+        const newCanvas = document.createElement('canvas');
+        newCanvas.setAttribute('id', 'image-heatmap-canvas');
+        canvas.parentElement.appendChild(newCanvas);
+        const ctx = newCanvas.getContext('2d');
+        const background = new Image();
+        background.src = this.imageUrl;
+        const result: number[] = [];
+        let width = 0;
+        let height = 0;
+        const self = this;
+        background.onload = function () {
+          width += background.width;
+          height += background.height;
+          self.heatmapImageEdgePoint.x = self.heatMapWidth - self.heatmapOffsetX * 2;
+          self.heatmapImageEdgePoint.y = self.heatMapHeight - self.heatmapOffsetY * 2;
+          ctx.drawImage(background, self.heatmapOffsetX, self.heatmapOffsetY,
+            self.heatmapImageEdgePoint.x, self.heatmapImageEdgePoint.y);
+          result.push(width);
+          result.push(height);
+          resolve(result);
+        }.bind(self);
+        newCanvas.setAttribute('width', `${this.heatMapWidth}px`);
+        newCanvas.setAttribute('height', `${this.heatMapHeight}px`);
+      });
   }
 
   private loadData(): void {
-    const data: number[][] = this.mockGenerateData();
+      const data: number[][] = this.mockGenerateData();
     const hours = new Date().getHours();
     let minutes: string = new Date().getMinutes().toString();
     minutes = parseInt(minutes, 10) > 9 ? minutes : `0${minutes}`;
@@ -156,6 +179,7 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
     };
     this.reportService.getCoordinates(request).first().subscribe((payload: CoordinatesIncident[]): void => {
       let i = true;
+      console.log(payload.length);
       payload.forEach((incident: CoordinatesIncident) => {
         if (i) {
           i = false;
@@ -170,19 +194,19 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   private mockGenerateData(): number[][] {
-    const data = [];
-    const xData = [];
-    const yData = [];
-    for (let i = 0; i <= 200; i++) {
-      for (let j = 0; j <= 200; j++) {
-        data.push([i, j, data.push([i, j, (i + j) * (Math.random() * 10) - 500])]);
+      const data = [];
+      const xData = [];
+      const yData = [];
+      for (let i = 0; i <= 200; i++) {
+        for (let j = 0; j <= 200; j++) {
+          data.push([i, j, data.push([i, j, (i + j) * (Math.random() * 10) - 500])]);
+        }
+        xData.push(i);
       }
-      xData.push(i);
+      for (let j = 0; j < 200; j++) {
+        yData.push(j);
+      }
+      return [xData, yData, data];
     }
-    for (let j = 0; j < 200; j++) {
-      yData.push(j);
-    }
-    return [xData, yData, data];
-  }
 
-}
+  }
