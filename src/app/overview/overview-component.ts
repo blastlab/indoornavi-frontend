bbab4c6d6fadf4bb637a978a52ecb51c48371a3e
bartoshko <blenart@blastlab.co>
2018-12-19 10:50:34 +0100

    map img and heatmap in the same size, binded ofset and box size navi-534

--- src/app/overview/overview-component.ts
+++ src/app/overview/overview-component.ts
@@ -19,10 +19,15 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
 
   floor: Floor;
   echartsInstance: any;
+  heatMapWidth = 1200;
+  heatMapHeight = 800;
+  private heatmapOffsetX: number;
+  private heatmapOffsetY: number;
   private imageUrl: string;
+  private imageWidth: number;
+  private imageHeight: number;
   private scale: Scale;
   private scaleCalculations: ScaleCalculations;
-  private d3map: MapSvg = null;
   private heatmap: DrawBuilder;
   private subscriptionDestructor: Subject<void> = new Subject<void>();
   chartOptions: EChartOption = {
@@ -41,41 +46,32 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
       calculable: true,
       realtime: false,
       inRange: {
-        color: ['rgba(22,0,229,0.4)', 'rgba(42,196,252,0.4)', 'rgba(76,239,136,0.4)', 'rgba(238,255,50,0.4)', 'rgba(255,206,30,0.4)', 'rgba(225,156,30,0.4)', 'rgba(225,109,30,0.4)', 'rgba(225,64,30,0.4)', 'rgba(225,64,3,0.4)', 'rgba(225,64,229,0.4)', 'rgba(225,0,0,0.4)']
+        color: [
+          'rgba(22,0,229,0.4)',
+          'rgba(42,196,252,0.4)',
+          'rgba(76,239,136,0.4)',
+          'rgba(238,255,50,0.4)',
+          'rgba(255,206,30,0.4)',
+          'rgba(225,156,30,0.4)',
+          'rgba(225,109,30,0.4)',
+          'rgba(225,64,30,0.4)',
+          'rgba(225,64,3,0.4)',
+          'rgba(225,64,229,0.4)',
+          'rgba(225,0,0,0.4)'
+        ]
       }
     },
     series: [{
       name: 'Time span',
       type: 'heatmap',
       data: [],
-      itemStyle: {
-        emphasis: {
-          borderColor: '#333',
-          borderWidth: 1
-        }
-      },
-      progressive: 1000,
-      animation: true
+      pointSize: 1,
+      blurSize: 6,
+      animation: false
     }]
   };
 
   @ViewChild('canvasParent') canvasParent;
-
-  static mockGenerateData(): number[][] {
-    const data = [];
-    const xData = [];
-    const yData = [];
-    for (let i = 0; i <= 200; i++) {
-      for (let j = 0; j <= 200; j++) {
-        data.push([i, j, data.push([i, j, (i + j) * (Math.random() * 10) - 1000])]);
-      }
-      xData.push(i);
-    }
-    for (let j = 0; j < 200; j++) {
-      yData.push(j);
-    }
-    return [xData, yData, data];
-  }
 
   constructor(private route: ActivatedRoute,
               private floorService: FloorService,
@@ -85,6 +81,8 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit() {
+    this.heatmapOffsetX = this.heatMapWidth * 0.1;
+    this.heatmapOffsetY = this.heatMapHeight * 0.115;
     this.setCorrespondingFloorParams();
     this.translateService.setDefaultLang('en');
   }
@@ -137,10 +135,14 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
           };
         }
         if (!!floor.imageId) {
-            this.mapService.getImage(floor.imageId).subscribe((blob: Blob) => {
-              this.imageUrl = URL.createObjectURL(blob);
+          this.mapService.getImage(floor.imageId).subscribe((blob: Blob) => {
+            this.imageUrl = URL.createObjectURL(blob);
+            this.loadMapImage().then((imageSize: number[]): void => {
+              this.imageHeight = imageSize[0];
+              this.imageWidth = imageSize[1];
               this.loadData();
             });
+          });
             // this.heatmap
             //   .createGroup()
             //   .injectHtml();
@@ -149,25 +151,56 @@ export class OverviewComponent implements OnInit, OnDestroy, AfterViewInit {
     });
   }
 
+  private loadMapImage(): Promise<number[]> {
+    return new Promise((resolve) => {
+      const canvas = document.getElementsByTagName('canvas').item(0);
+      const newCanvas = document.createElement('canvas');
+      newCanvas.setAttribute('id', 'image-heatmap-canvas');
+      canvas.parentElement.appendChild(newCanvas);
+      const ctx = newCanvas.getContext('2d');
+      const background = new Image();
+      background.src = this.imageUrl;
+      const result: number[] = [];
+      let width = 0;
+      let height = 0;
+      const self = this;
+      background.onload = function () {
+        width += background.width;
+        height += background.height;
+        ctx.drawImage(background, self.heatmapOffsetX, self.heatmapOffsetX,
+          self.heatMapWidth - self.heatmapOffsetX * 2, self.heatMapHeight - self.heatmapOffsetY * 2);
+        result.push(width);
+        result.push(height);
+        resolve(result);
+      }.bind(self);
+      newCanvas.setAttribute('width', `${this.heatMapWidth}px`);
+      newCanvas.setAttribute('height', `${this.heatMapHeight}px`);
+    });
+  }
+
   private loadData(): void {
-    const data: number[][] = OverviewComponent.mockGenerateData();
+    const data: number[][] = this.mockGenerateData();
     this.chartOptions.xAxis.data = data[0];
     this.chartOptions.yAxis.data = data[1];
     this.chartOptions.series[0].data = data[2];
     this.chartOptions = Object.assign({}, this.chartOptions);
-    const canvas = document.getElementsByTagName('canvas').item(0);
-    const newCanvas = document.createElement('canvas');
-    newCanvas.setAttribute('id', 'image-heatmap-canvas');
-    canvas.parentElement.appendChild(newCanvas);
-    const ctx = newCanvas.getContext('2d');
-    const background = new Image();
-    background.src = this.imageUrl;
-    background.onload = function () {
-      ctx.drawImage(background, 0, 0);
-    };
-    newCanvas.setAttribute('width', '2200px');
-    newCanvas.setAttribute('height', '1200px');
-    // canvas.parentNode.insertBefore(newCanvas, canvas.nextSibling);
-    // this.canvasParent.nativeElement.appendChild()
   }
+
+  private mockGenerateData(): number[][] {
+    console.log(this.imageHeight, this.imageHeight);
+    const data = [];
+    const xData = [];
+    const yData = [];
+    for (let i = 0; i <= 100; i++) {
+      for (let j = 0; j <= 100; j++) {
+        data.push([i, j, data.push([i, j, (i + j) * (Math.random() * 10) - 500])]);
+      }
+      xData.push(i);
+    }
+    for (let j = 0; j < 100; j++) {
+      yData.push(j);
+    }
+    return [xData, yData, data];
+  }
+
 }
