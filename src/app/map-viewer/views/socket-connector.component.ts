43651304605ab8eb7a43977d4a8ced81138decb5
bartoshko <blenart@blastlab.co>
2019-06-19 06:49:55 +0200

    point to calculator endpoint for measurements, navi-655

--- src/app/map-viewer/views/socket-connector.component.ts
+++ src/app/map-viewer/views/socket-connector.component.ts
@@ -264,7 +264,7 @@ export class SocketConnectorComponent implements OnInit, OnDestroy, AfterViewIni
 
   protected initializeSocketConnection(): void {
     this.ngZone.runOutsideAngular((): void => {
-      const stream = this.socketService.connect(`${Config.WEB_SOCKET_URL}measures?${Config.WS_KEY_FRONTEND}`);
+      const stream = this.socketService.connect(`${Config.CALCULATOR_URL}measures?${Config.WS_KEY_FRONTEND}`);
       this.setSocketConfiguration();
       this.tagToggleService.onToggleTag().takeUntil(this.subscriptionDestructor).subscribe((tagToggle: TagToggle) => {
         this.socketService.send({type: CommandType[CommandType.TOGGLE_TAG], args: tagToggle.tag.shortId});
